a8ee8bb42ff773fc6881a6115b021f5dd34448c1
null-Exception1 <rising1legend@gmail.com>
2026-07-01 14:52:04 +0530

    added goroutine ratelimiting

--- golang/server.go
+++ golang/server.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"strconv"
+	"time"
 )
 
 type Movie struct {
@@ -13,45 +14,76 @@ type Movie struct {
 	Rating   int    `json:"rating"`
 }
 
+type Request struct {
+	req_type int
+	movie    Movie
+	w        http.ResponseWriter
+}
+
 var movies []Movie
+var ticker = time.NewTicker(200 * time.Millisecond)
+var ratelimit chan time.Time
+var queue chan time.Time
 
 func GetHandler(w http.ResponseWriter, res *http.Request) {
-	fmt.Println("GET", movies)
 
+	fmt.Println("[GET]", movies)
 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Set("Content-Type", "application/json")
-
 	str, _ := json.Marshal(movies)
-
-	fmt.Fprintf(w, string(str))
-
+	fmt.Fprintf(w, "%s", string(str))
 }
 func SetHandler(w http.ResponseWriter, res *http.Request) {
-	queryParams := res.URL.Query()
 
 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
 	w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 	w.Header().Set("Content-Type", "application/json")
-
+	queryParams := res.URL.Query()
 	name := queryParams.Get("name")
 	duration, _ := strconv.Atoi(queryParams.Get("duration"))
 	rating, _ := strconv.Atoi(queryParams.Get("rating"))
 
 	toadd := Movie{name, duration, rating}
-	//fmt.Println(movies)
+
 	movies = append(movies, toadd)
-	//fmt.Println(movies)
-	fmt.Println("New add req", name, duration, rating)
+
+	fmt.Println("[ADD]", name, duration, rating)
 	fmt.Fprintf(w, "true")
 }
+func RateLimit(handlerfunc http.HandlerFunc) http.HandlerFunc {
+	//queue <- time.Now()
+	return func(w http.ResponseWriter, res *http.Request) {
+		select {
+		case <-ratelimit: // wait for if a chance is in ratelimit or not (max 5)
+			handlerfunc(w, res)
+		default:
+			fmt.Println("YO 2 many requests")
+		}
+
+	}
+}
+func Routine() {
+	for range ticker.C {
+		select {
+		case ratelimit <- time.Now(): // try to give chance for a request to snap on ratelimit and take it
+		default: // but only every tick and only if ratelimit isnt full
+		}
+	}
+}
+
+/*
+-> the request reaches the ratelimit
+-> routine delays ratelimit until there is room
+*/
 func main() {
 	movies = make([]Movie, 0)
-
-	http.HandleFunc("/get", GetHandler)
-	http.HandleFunc("/add", SetHandler)
+	ratelimit = make(chan time.Time, 5)
+	go Routine()
+	http.HandleFunc("/get", RateLimit(GetHandler))
+	http.HandleFunc("/add", RateLimit(SetHandler))
 
 	http.ListenAndServe("localhost:8080", nil)
 }
